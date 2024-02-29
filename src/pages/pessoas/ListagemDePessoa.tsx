24bad3f68ded85e7886af1ccbdb7b8ee5873eaab
Davidsonc <davidsonc@gmail.com>
2024-02-29 02:28:32 -0300

    feat: listagem de pessoas isLoading e listagem vazia

--- src/pages/pessoas/ListagemDePessoa.tsx
+++ src/pages/pessoas/ListagemDePessoa.tsx
@@ -1,31 +1,42 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 
-import { PessoasService } from '../../shared/services/pessoas/PessoasService';
+import { IListagemPessoa, PessoasService } from '../../shared/services/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { useDebounce } from '../../shared/hooks';
-
+import { Environment } from '../../shared/environment';
 
 
 export const ListagemDePessoa: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  
-  const { deBounce } =  useDebounce(3000);
+
+  const { deBounce } = useDebounce();
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalCount, setTotalCount] = useState(0);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     deBounce(() => {
-      PessoasService.getAll( 1, busca)
+      PessoasService.getAll(1, busca)
         .then((result) => {
+          setIsLoading(false);
           if (result instanceof Error) {
             alert(`Erro: ${result.message}`);
             return;
           }
           console.log(result);
+
+          setTotalCount(result.totalCount);
+          setRows(result.data);
         });
     });
 
@@ -40,8 +51,42 @@ export const ListagemDePessoa: React.FC = () => {
         mostrarInputBusca
         textoDaBusca={busca}
         textoBotaoNovo='Nova'
-        aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto}, {replace: true})}
+        aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
       />
+
+      <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </LayoutBaseDePagina>
   );
 };
